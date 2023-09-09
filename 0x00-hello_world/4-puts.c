b620ac3768e8372e6309875ab5b87270cd308ef4
ImeGits <udomary963@gmail.com>
2023-09-09 13:35:26 -0700

    cisfun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-/*
- * main -Entry point
+/**
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingualpuzzule");
+	puts("\"Programming is like building a multilingualpuzzle");
 	return (0);
 }
