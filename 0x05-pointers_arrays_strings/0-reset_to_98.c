5edd43733815a9462ed7b35efe8d1c86b1844cf9
ImeGits <udomary963@gmail.com>
2023-09-18 12:58:17 -0700

    cisfun

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that takes a pointer to an integer parameter
+ * @n: integer pointer
+ * Return: 0 Success
+ */
+
+void reset_to98(int *n)
+{
+	*n = 98;
+}
