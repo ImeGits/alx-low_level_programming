8809e9bf2915abb337206565049826a01d30ff98
ImeGits <udomary963@gmail.com>
2023-10-10 16:31:30 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+/**
+ * init_dog - initialized function of a type of dog.
+ * @d: pointer function
+ * @name: pointer of the dog type name
+ * @age: The dog type age
+ * @owner: pointer to the owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+
+		d->age = age;
+
+		d->owner = owner;
+	}
+}
