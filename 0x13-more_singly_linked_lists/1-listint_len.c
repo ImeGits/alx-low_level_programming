9a6d017a9e03149a1f5189a6eaf0d5e4c559b060
ImeGits <udomary963@gmail.com>
2023-10-24 15:36:53 -0700

    more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements on a linked list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t dodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		dodes++;
+	}
+	return (doded);
+}
