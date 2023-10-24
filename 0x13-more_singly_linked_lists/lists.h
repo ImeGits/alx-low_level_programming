89a896cba284561b96f6191bf4f82f2706dc2e7c
ImeGits <udomary963@gmail.com>
2023-10-24 15:20:29 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,8 +14,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 /**
@@ -29,7 +29,7 @@ typedef struct listp_s
 {
 	void *p;
 	struct listp_s *next;
-}listp_t;
+} listp_t;
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
