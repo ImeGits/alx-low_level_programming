4ea1eb07589d4755a497fc6540941d0bdd166908
ImeGits <udomary963@gmail.com>
2023-12-10 15:44:03 -0800

    doubly_linked_lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,18 +1,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a dlistint_t list
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ *
  * @h: head of the list
  * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int cal;
+	int count;
 
-	cal = 0;
-	
+	count = 0;
+
 	if (h == NULL)
-		return (cal);
+		return (count);
 
 	while (h->prev != NULL)
 		h = h->prev;
@@ -20,8 +22,9 @@ size_t print_dlistint(const dlistint_t *h)
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		cal++;
+		count++;
 		h = h->next;
 	}
-	return (cal);
+
+	return (count);
 }
